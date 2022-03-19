08a89b5fa9811943b32e25f1a5dd033c655f3827
Hugo Mota <hugo.iguanaa@gmail.com>
2022-03-19 14:43:08 +0000

    Update minikube.ts

--- kubernetes/minikube.ts
+++ kubernetes/minikube.ts
@@ -1,6 +1,18 @@
 
-$ minikube status
-$ minikube start --driver=virtualbox
-$ minikube start --driver=docker
-$ minikube start --driver=hyperv
-$ minikube service [service-name]
+# Documentation:
+https://minikube.sigs.k8s.io/docs/start/
+
+# Main commands:
+minikube status
+minikube start --driver=virtualbox
+minikube start --driver=docker
+minikube start --driver=hyperv
+
+# To make virtualbox the default driver:
+minikube config set driver virtualbox
+
+minikube service [service-name]
+minikube delete --all
+
+
+minikube dashboard
